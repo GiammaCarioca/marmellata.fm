1d05d53b5cd198645ae919c8a892a33da97f8754
Thiago Giammattey <giammattey@gmail.com>
2020-06-08 15:25:50 -0300

    using mixcloud’s audio widget library

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* global Mixcloud*/
+import React, { useRef, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import FeaturedMix from './FeaturedMix'
@@ -8,43 +9,60 @@ const Home = () => <h1>Home</h1>
 const Archive = () => <h1>Archive</h1>
 const About = () => <h1>About</h1>
 
-const App = () => (
-	<Router>
-		<div>
-			<div className="flex-l justify-end">
-				{/* FeaturedMix */}
-				<FeaturedMix />
+const App = () => {
+	const playerRef = useRef()
 
-				<div className="w-50-l relative z-1">
-					{/* Header */}
-					<Header />
+	useEffect(() => {
+		const widget = Mixcloud.PlayerWidget(playerRef.current)
 
-					{/* Routed page */}
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route path="/archive">
-							<Archive />
-						</Route>
-						<Route path="/about">
-							<About />
-						</Route>
-					</Switch>
+		// widget.ready.then((widget) => widget.play())
+
+		const setupWidget = async (widget) => {
+			await widget.ready
+			widget.play()
+		}
+
+		setupWidget(widget)
+	}, []) // the callback will only be fired once, similar to componentDidMount
+
+	return (
+		<Router>
+			<div>
+				<div className="flex-l justify-end">
+					{/* FeaturedMix */}
+					<FeaturedMix />
+
+					<div className="w-50-l relative z-1">
+						{/* Header */}
+						<Header />
+
+						{/* Routed page */}
+						<Switch>
+							<Route exact path="/">
+								<Home />
+							</Route>
+							<Route path="/archive">
+								<Archive />
+							</Route>
+							<Route path="/about">
+								<About />
+							</Route>
+						</Switch>
+					</div>
 				</div>
+
+				{/* AudioPlayer */}
+				<iframe
+					title="mixcloud"
+					width="100%"
+					height="60"
+					src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+					frameBorder="0"
+					className="db fixed bottom-0 z-5"
+					ref={playerRef}
+				/>
 			</div>
-
-			{/* AudioPlayer */}
-			<iframe
-				title="mixcloud"
-				width="100%"
-				height="60"
-				src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
-				frameBorder="0"
-				className="db fixed bottom-0 z-5"
-			/>
-		</div>
-	</Router>
-)
-
+		</Router>
+	)
+}
 export default App
