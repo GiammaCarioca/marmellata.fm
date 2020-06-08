5a4e89053aa7ea5a287077676672efb14e271889
Thiago Giammattey <giammattey@gmail.com>
2020-06-08 09:46:14 -0300

    adding Header and FeaturedMix components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-function App() {
-	return (
+import FeaturedMix from './FeaturedMix'
+import Header from './Header'
+
+const App = () => (
+	<div>
 		<div>
-			<h1>Marmellata.fm</h1>
+			{/* FeaturedMix (needs styling and updating) */}
+			<FeaturedMix />
+
+			<div>
+				{/* Header (needs styling and updating) */}
+				<Header />
+
+				{/* Routed page */}
+			</div>
 		</div>
-	)
-}
+
+		{/* AudioPlayer */}
+		<iframe
+			title="mixcloud"
+			width="100%"
+			height="60"
+			src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+			frameBorder="0"
+		/>
+	</div>
+)
 
 export default App
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedMix = () => (
+	<div>
+		<p>Featured mix</p>
+		<h1>Mint Condition w/ Hotthobo - 27th November 2017</h1>
+		{/* PlayButton */}
+	</div>
+)
+
+export default FeaturedMix
