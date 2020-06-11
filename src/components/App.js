90a433f69e07464f8872ed13c21d7a3b129fd355
Thiago Giammattey <giammattey@gmail.com>
2020-06-11 18:05:26 -0300

    playing around with mixcloud api

--- src/components/App.js
+++ src/components/App.js
@@ -18,38 +18,66 @@ const App = () => {
 	useEffect(() => {
 		const widget = Mixcloud.PlayerWidget(playerRef.current)
 
+		const initialMix = playerRef.current.id
+
+		// get the mix from the iframe and set it to state
+		setCurrentMix(initialMix)
+
 		// widget.ready.then((widget) => widget.play())
 
 		const setupWidget = async (widget) => {
+			// wait until the widget is ready
 			await widget.ready
 
 			// store the widget inside state so it's available outside this function
 			setWidget(widget)
-
-			// start playing the mix immediately
-			// widget.play()
 
 			// using the mixcloud widget events we can detect when our audio has been paused, set playing state to false
 			widget.events.pause.on(() => setPlaying(false))
 
 			//audio is playing again, so we set playing state to true
 			widget.events.play.on(() => setPlaying(true))
+
+			// start playing the mix immediately; inconsistent
+			widget.play()
 		}
 
 		setupWidget(widget)
 	}, []) // the callback will only be fired once, similar to componentDidMount
 
-	const togglePlay = () => {
-		if (!widget) return
-		widget.togglePlay()
-	}
-
 	const playMix = (mixName) => {
-		// update the currentMix in our state with the mixName
-		setCurrentMix(mixName)
+		// widget.getIsPaused().then((paused) => console.log('is paused?', paused))
 
-		// load a new mix by its name and then start playing it immediately
-		widget.load(mixName, true)
+		if (!widget) return
+
+		if (currentMix === mixName) {
+			// update the currentMix in our state with the mixName
+			// setCurrentMix(mixName) // asynchronous!!!
+
+			console.log('equal?', currentMix === mixName)
+			widget.togglePlay()
+
+			widget.events.pause.on(() => setPlaying(false)) // asynchronous!!!
+			widget.events.play.on(() => setPlaying(true)) // asynchronous!!!
+
+			return
+		}
+
+		if (currentMix !== mixName) {
+			console.log('equal?', currentMix === mixName)
+			console.log('play this!', mixName)
+
+			// update the currentMix in our state with the mixName
+			setCurrentMix(mixName) // asynchronous!!!
+
+			// load a new mix by its name and then start playing it immediately
+			widget.load(mixName, true)
+
+			widget.events.pause.on(() => setPlaying(false)) // asynchronous!!!
+			widget.events.play.on(() => setPlaying(true)) // asynchronous!!!
+
+			return
+		}
 	}
 
 	return (
@@ -66,17 +94,12 @@ const App = () => {
 						{/* Routed page */}
 
 						<div>
-							<button onClick={() => togglePlay()}>
-								{playing ? 'Pause' : 'Play'}
-							</button>
-						</div>
-
-						<div>
-							<h1>current playing: {currentMix}</h1>
+							{!!currentMix && <h1>current mix: {currentMix}</h1>}
+							{playing ? <p>playing</p> : <p>paused</p>}
 							<button
 								onClick={() => playMix('/NTSRadio/bonobo-24th-june-2015/')}
 							>
-								Play bonobo mix
+								'Play/Pause bonobo mix'
 							</button>
 
 							<button
@@ -84,7 +107,7 @@ const App = () => {
 									playMix('/NTSRadio/floating-points-four-tet-16th-march-2017/')
 								}
 							>
-								Play four tet mix
+								Play/Pause four tet mix'
 							</button>
 						</div>
 
@@ -110,6 +133,7 @@ const App = () => {
 					src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
 					frameBorder="0"
 					className="db fixed bottom-0 z-5"
+					id="/NTSRadio/floating-points-jamie-xx-18th-august-2016/"
 					ref={playerRef}
 				/>
 			</div>
