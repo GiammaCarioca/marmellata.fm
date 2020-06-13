3a19c506aabddd3ea549d07166bb96117aa3ee5d
Thiago Giammattey <giammattey@gmail.com>
2020-06-13 17:16:32 -0300

    creating a stat component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const Stat = ({ statName, statNumber, statWord }) => (
+	<div
+		className="w-third tc pa3 ba bw2 b--light-gray"
+		style={{ marginRight: -2 }}
+	>
+		<div className="f6 biryani ttu">{statName}</div>
+		<div className="f5 b biryani-black ttu tracked">
+			{statNumber} {statWord}
+		</div>
+	</div>
+)
+
 const About = () => (
 	<div className="ph3 ph4-l">
 		<div className="measure center lh-copy f4 ph3">
@@ -11,6 +23,12 @@ const About = () => (
 				Whether you’re into hip hop, trip hop, classic jazz, fusion jazz, afro
 				beat or break beat… we have you covered!
 			</p>
+		</div>
+
+		<div className="flex pt3">
+			<Stat statName="Featuring..." statNumber={11} statWord="mixes" />
+			<Stat statName="Played..." statNumber={114746} statWord="times" />
+			<Stat statName="With..." statNumber={64851} statWord="seconds" />
 		</div>
 	</div>
 )
